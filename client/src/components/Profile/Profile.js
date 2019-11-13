0e0a44d71d234f5ac606b74835f736f19ff378bd
unknown <alexander.norberghoff@gmail.com>
2019-11-13 13:15:25 +0100

    added httpGet hook and populated profile with real data

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import Header from '../UI/Header/Header'
+import useHttpGet from '../../hooks/useHttpGet'
 const testAvatar = "https://scontent-arn2-2.cdninstagram.com/vp/fc9f9cdae239fe0319afc0cca853cd2d/5E5792C1/t51.2885-19/s150x150/66230601_898875400450053_5296268938865278976_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com"
 const Profile = () => {
+   const { data, error, loading } = useHttpGet('/profiles')
+
+   if (loading) {
+      return <div className="profile"></div>
+   }
+
+   const { username, followers, followees, post_count, profile_text } = data || {}
    return (
       <>
          <Header />
          <div className="profile">
+            {error && <p className="error">Something went wrong</p>}
             <header className="profile__header">
                <div className="profile__header__avatar">
-                  <img src={testAvatar} alt="avatar"/>
+                  <img src={testAvatar} alt="avatar" />
                </div>
                <div className="profile__header__info">
                   <div className="profile__header__info--first">
-                     <p className="lead">Test</p>
+                     <p className="lead"> {username} </p>
                      <button className="btn btn--primary">Follow</button>
                   </div>
                   <div className="profile__header__info--second">
-                     <p className="lead"><strong>0</strong> posts</p>
-                     <p className="lead"><strong>22k</strong> followers</p>
-                     <p className="lead"><strong>105</strong> following</p>
+                     <p className="lead"><strong>{post_count}</strong> posts</p>
+                     <p className="lead"><strong> {followers} </strong> followers</p>
+                     <p className="lead"><strong> {followees} </strong> following</p>
                   </div>
                   <div className="profile__header__info--third">
                      <p className="lead">
-                        This is a very descriptive text about me!
+                        {profile_text}
                      </p>
                   </div>
                </div>
---
+++ client/src/hooks/useHttpGet.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import axios from '../axios/axios'
+
+export default initUrl => {
+   const [url, setUrl] = useState(initUrl)
+   const [data, setData] = useState()
+   const [error, setError] = useState(null)
+   const [loading, setLoading] = useState(true)
+   useEffect(() => {
+      console.log('ran')
+      axios.get(url)
+         .then(({ data }) => {
+            setData(data)
+            setLoading(false)
+         })
+         .catch(err => {
+            setError(err)
+            setLoading(false)
+         })
+   }, [url])
+
+   return {
+      data,
+      error,
+      loading,
+      setUrl
+   }
+}
