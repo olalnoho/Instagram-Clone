9f132b51d7601f4e20f0f52eb5aada406c730fc3
unknown <alexander.norberghoff@gmail.com>
2019-11-13 14:39:20 +0100

    can now visit other profiles

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from './components/UI/Landing/Landing';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Profile from './components/Profile/Profile';
+import OtherProfile from './components/OtherProfile/OtherProfile';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" exact component={Landing} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/profiles/:username" component={OtherProfile} />
       </Switch>
     </div>
   )
---
+++ client/src/components/OtherProfile/OtherProfile.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+import Header from '../UI/Header/Header'
+import useHttpGet from '../../hooks/useHttpGet'
+const testAvatar = "https://scontent-arn2-2.cdninstagram.com/vp/fc9f9cdae239fe0319afc0cca853cd2d/5E5792C1/t51.2885-19/s150x150/66230601_898875400450053_5296268938865278976_n.jpg?_nc_ht=scontent-arn2-2.cdninstagram.com"
+const OtherProfile = (props) => {
+   const uname = props.match.params.username
+   const { user } = useContext(AuthContext)
+
+   const { data, error, loading } = useHttpGet(`/profiles/${uname}`)
+
+   if (loading) {
+      return <div className="profile"></div>
+   }
+
+   if (user && user.username === uname) {
+      return <Redirect to="/profile" />
+   }
+
+   const { username, followers, followees, post_count, profile_text } = data || {}
+   return (
+      <>
+         <Header />
+         <div className="profile">
+            {error ? <p className="error">Could not find user {uname}</p> :
+               <header className="profile__header">
+                  <div className="profile__header__avatar">
+                     <img src={testAvatar} alt="avatar" />
+                  </div>
+                  <div className="profile__header__info">
+                     <div className="profile__header__info--first">
+                        <p className="lead"> {username} </p>
+                        <button className="btn btn--primary">Follow</button>
+                     </div>
+                     <div className="profile__header__info--second">
+                        <p className="lead"><strong>{post_count}</strong> posts</p>
+                        <p className="lead"><strong> {followers} </strong> followers</p>
+                        <p className="lead"><strong> {followees} </strong> following</p>
+                     </div>
+                     <div className="profile__header__info--third">
+                        <p className="lead">
+                           {profile_text}
+                        </p>
+                     </div>
+                  </div>
+               </header>}
+         </div>
+      </>
+   )
+}
+
+export default OtherProfile
