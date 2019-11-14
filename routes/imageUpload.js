753f8be509a16f5ada274c0d820bba420efb69e8
unknown <alexander.norberghoff@gmail.com>
2019-11-14 14:59:30 +0100

    clean up

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const fs = require('fs').promises
 const path = require('path')
+
 router.post('/', async (req, res) => {
    const { file } = req.files || {}
    const username = req.body.username
-   if (!file || !username) {
+
+   if (!file) {
       return res.status(400).json({
          err: 'No file'
       })
@@ -13,12 +15,16 @@ router.post('/', async (req, res) => {
    let folder = path.join(__dirname, '../public', username)
    let fullPath = path.resolve(folder, file.name)
    try {
-      const asd = await fs.access(folder)
+      await fs.access(folder)
       await file.mv(fullPath)
    } catch (err) {
       if (err.code === 'ENOENT') {
          await fs.mkdir(folder, { recursive: true })
          await file.mv(fullPath)
+      } else {
+         return res.status(500).json({
+            err: 'Server error'
+         })
       }
    }
    return res.send('Ok')
