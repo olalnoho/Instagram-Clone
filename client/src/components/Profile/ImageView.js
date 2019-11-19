9a10e5fb51c54285bb501af714d973ae94a91b45
unknown <alexander.norberghoff@gmail.com>
2019-11-19 14:49:17 +0100

    slighly better way to do autoscroll

--- client/src/components/Profile/ImageView.js
+++ client/src/components/Profile/ImageView.js
@@ -3,23 +3,24 @@ import useHttpGet from '../../hooks/useHttpGet'
 import useHttpPost from '../../hooks/useHttp'
 import { AuthContext } from '../../context/AuthContext'
 const ImageView = ({ photo, avatar, username, id }) => {
-
-   const scrollDiv = useRef()
-
-   const { data: comments,
+   const {
+      data: comments,
       loading: commentLoad,
       setData: setComments
    } = useHttpGet(`/photos/${id}`)
 
    const { fetchData } = useHttpPost(`/photos/${id}`)
 
+   const scrollDiv = useRef()
    const { user } = useContext(AuthContext)
+
    const [loading, setLoading] = useState(true)
    const [commentText, setCommentText] = useState('')
    const [mWidth, setMWidth] = useState(935)
    const [hasScrolled, setHasScrolled] = useState(false)
 
    const imgLoad = ({ target }) => {
+      // For resizing min width on Image modal
       if ((target.width + 300) < 935) {
          setMWidth(target.width + 300)
       }
@@ -40,19 +41,13 @@ const ImageView = ({ photo, avatar, username, id }) => {
    }, [scrollDiv])
 
    useEffect(() => {
-      // It's wrapped in STO for event queue
-      setTimeout(() => {
-         const box = document.querySelector('.imageview__right-comments')
-         if (!hasScrolled || shouldAutoScroll(box)) {
-            box.scrollTop = box.scrollHeight
-         }
-      }, 0)
-
-   }, [comments, hasScrolled])
+      if (!hasScrolled || shouldAutoScroll(scrollDiv.current)) {
+         scrollDiv.current.scrollTop = scrollDiv.current.scrollHeight
+      }
+   }, [hasScrolled, commentLoad, loading, comments])
 
    const shouldAutoScroll = box => {
-      // 400 is arbitrary and something i
-      // landed on after some trial and error
+      // 400 is arbitrary
       if (box.scrollHeight - (box.scrollTop + box.offsetHeight) < 400) {
          return true
       }
@@ -82,17 +77,19 @@ const ImageView = ({ photo, avatar, username, id }) => {
          </div>
          <div className="imageview__right">
             <header className="imageview__right-header">
-               <img src={avatar} alt="avatar" />
+               <img src={avatar + '?' + new Date().getTime()} alt="avatar" />
                <span> {username}  </span>
             </header>
             <div className="imageview__right-comments" ref={scrollDiv}>
                {commentLoad ? <p>Loading...</p> :
                   comments.map(comment => {
                      return <div key={comment.created_at} className="comment">
-                        <img src={`http://localhost:5000/${comment.avatar}`} alt="avatar" />
-                        <strong className="name">
-                           {comment.username}
-                        </strong>
+                        <div className="comment__user">
+                           <img src={`http://localhost:5000/${comment.avatar}?${new Date().getTime()}`} alt="avatar" />
+                           <strong className="name">
+                              {comment.username}
+                           </strong>
+                        </div>
                         <p className="lead"> {comment.comment} </p>
                      </div>
                   })
